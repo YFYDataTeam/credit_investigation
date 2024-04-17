99eb00f2cae6a48b976554f84a0477482be52466
sean.chang <sean.chang@yfy.com>
2024-04-17 14:17:21 +0800

    feature/issue_branch2 frontend refactor

--- frontend/src/MainPage/BasicInfo.jsx
+++ frontend/src/MainPage/BasicInfo.jsx
@@ -11,16 +11,69 @@ const BasicInfo = ({companyId}) => {
             try {
                 console.log("input", companyId);
                 const response = await fetch(`${endpoint}basicinfo/${companyId}`);
+
+                if(!response.ok){
+                    throw new Error("Data not found.");
+                }
+
+                const data = await response.josn();
+
+                if (data.message === "NoData"){
+                    throw Error("displayNoDataMessage");
+                } else {
+                    setBasicInfo(data);
+                }
+                
+
             } catch (error) {
                 console.error("Error:", error)
             }
         };
 
+
         fetch();
-    }, []);
+    // whenever companyId changes,
+    // Dependency Array: [companyId] is the dependency array for this useEffect. 
+    // This array tells React to keep track of the variables listed inside it (in this case, just companyId). 
+    }, [companyId]);
     
 
-    return 
+    return (
+        <Container title="公司基本資訊">
+          {basicInfo && (
+            <div>
+              <div class="info-row">
+                <div class="info-column">
+                  <h3>公司名稱</h3>
+                  <p>{basicInfo.company_name}</p>
+                </div>
+                <div class="info-column">
+                  <h3>統一編號</h3>
+                  <p>{basicInfo.company_account}</p>
+                </div>
+                <div class="info-column">
+                  <h3>公司目前狀態</h3>
+                  <p>{basicInfo.company_status}</p>
+                </div>
+              </div>
+              <div class="info-row">
+                <div class="info-column">
+                  <h3>資本額</h3>
+                  <p>{basicInfo.company_captial}</p>
+                </div>
+                <div class="info-column">
+                  <h3>董事長</h3>
+                  <p>{basicInfo.chairman}</p>
+                </div>
+                <div class="info-column">
+                  <h3>董事</h3>
+                  <p>{basicInfo.directors}</p>
+                </div>
+              </div>
+            </div>
+          )}
+        </Container>
+      );
 };
 
 export default BasicInfo;
