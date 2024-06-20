75178575058088ac9eb0a08ff6d8570d5e2dd609
Wan Wan <lf2com.bt@gmail.com>
2024-06-20 23:09:26 +0800

    refactor: change AuthCheck to useAuthCheck

--- frontend/src/MainPage/index.jsx
+++ frontend/src/MainPage/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from "react";
 // import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
-import Container  from "./Container";
+import Container from "./Container";
 import BasicInfo from "./BasicInfo";
 import EpaReport from "./EpaReport";
 import PstReport from "./PstReport";
@@ -8,89 +8,81 @@ import FinancialReport from "./FinancialReports";
 import RevenueAnalysis from "./RevenueAnalysis";
 import CddResult from "./CddResult";
 import JudgementSummary from "./JudgementSummary";
-import AuthCheck from "./AuthCheck";
-import '../../assets/css/title_section.css';
+import useAuthCheck from "../hooks/useAuthCheck";
+import "../../assets/css/title_section.css";
 
 let endPoint = process.env.END_POINT;
 
 const App = () => {
-    const [companyId, setCompanyId] = useState("");
-    const [companyName, setCompanyName] = useState("");
-    const [isLoading, setIsLoading] = useState(false);
-    const [finalCompanyId, setFinalCompanyId] = useState("");
-    const [finalCompanyName, setFinalCompanyName] = useState("");
-    const [isValidUser, setIsValidUser] = useState(true); // Manage user validation status
-    const [isAuthValid, setIsAuthValid] = useState(null); // Manage auth validation status
+  const [companyId, setCompanyId] = useState("");
+  const [companyName, setCompanyName] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [finalCompanyId, setFinalCompanyId] = useState("");
+  const [finalCompanyName, setFinalCompanyName] = useState("");
+  const [isValidUser, setIsValidUser] = useState(true); // Manage user validation status
 
+  const isAuthValid = useAuthCheck();
 
-    const basicInfoRef = useRef(null); 
+  const basicInfoRef = useRef(null);
 
-    const handleSearchClick = async () => {
-        const company_id = companyId.trim();
-        try {
-            if (company_id === "") {
-                throw new Error("No company ID entered.");
-            }
-            setFinalCompanyId(company_id);
-        } catch (e) {
-            alert(e.message);
-        }
-    };
+  const handleSearchClick = async () => {
+    const company_id = companyId.trim();
+    try {
+      if (company_id === "") {
+        throw new Error("No company ID entered.");
+      }
+      setFinalCompanyId(company_id);
+    } catch (e) {
+      alert(e.message);
+    }
+  };
 
-    return (
-        <div>
-            <AuthCheck onValidation={setIsAuthValid} />
-
-            {isAuthValid === false ? (
-                // <Redirect to="/invalid-user" /> // redirect
-                <section className="welcome-hero">
-                <div className="header-text">
-                    <h1>徵信報告</h1>
-                    <p className="subheading">
-                        整合公司基本資訊、財部報表、公示資料等資料，快速評估公司運營情況
-                    </p>
-                </div>
-            </section>
-            ) : (
-                <>
-                    <section className="welcome-hero">
-                        <div className="header-text">
-                            <h1>徵信報告</h1>
-                            <p className="subheading">
-                                整合公司基本資訊、財部報表、公示資料等資料，快速評估公司運營情況
-                            </p>
-                        </div>
-                    </section>
-
-                    <Container title="輸入公司統編">
-                        <div className="input-container">
-                            <input 
-                                type="number" 
-                                value={companyId}
-                                onChange={(e) => setCompanyId(e.target.value)}
-                            />
-                            <button
-                                disabled={isLoading}
-                                onClick={handleSearchClick}
-                            >
-                                查詢
-                            </button>
-                        </div>
-                    </Container>
-
-                    {finalCompanyId && (
-                        <div ref={basicInfoRef}>
-                            <BasicInfo endPoint={endPoint} companyId={finalCompanyId} />
-                        </div>
-                    )}
-                    {finalCompanyId && <CddResult endPoint={endPoint} companyId={finalCompanyId} />}
-                    {finalCompanyId && <RevenueAnalysis endPoint={endPoint} companyId={finalCompanyId} />}
-                    {finalCompanyId && <FinancialReport endPoint={endPoint} companyId={finalCompanyId} />}
-                    {finalCompanyId && <JudgementSummary endPoint={endPoint} companyId={finalCompanyId} />}
-                </>
-            )}
+  return (
+    <div>
+      <section className="welcome-hero">
+        <div className="header-text">
+          <h1>徵信報告</h1>
+          <p className="subheading">
+            整合公司基本資訊、財部報表、公示資料等資料，快速評估公司運營情況
+          </p>
         </div>
-    );
+      </section>
+      {isAuthValid && (
+        <>
+          <Container title="輸入公司統編">
+            <div className="input-container">
+              <input
+                type="number"
+                value={companyId}
+                onChange={(e) => setCompanyId(e.target.value)}
+              />
+              <button disabled={isLoading} onClick={handleSearchClick}>
+                查詢
+              </button>
+            </div>
+          </Container>
+
+          {finalCompanyId && (
+            <div ref={basicInfoRef}>
+              <BasicInfo endPoint={endPoint} companyId={finalCompanyId} />
+            </div>
+          )}
+          {finalCompanyId && (
+            <CddResult endPoint={endPoint} companyId={finalCompanyId} />
+          )}
+          {finalCompanyId && (
+            <RevenueAnalysis endPoint={endPoint} companyId={finalCompanyId} />
+          )}
+          {finalCompanyId && (
+            <FinancialReport endPoint={endPoint} companyId={finalCompanyId} />
+          )}
+          {finalCompanyId && (
+            <JudgementSummary endPoint={endPoint} companyId={finalCompanyId} />
+          )}
+        </>
+      )}
+    </div>
+  );
 };
 
 export default App;
---
+++ frontend/src/hooks/useAuthCheck.js
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+
+const useAuthCheck = ({ onValidation }) => {
+  const [isValid, setIsValid] = useState(false);
+  const end_point = "https://yfy.ideaxpress.biz/api";
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJiYjViOWIxYy05YmNjLTQwZWYtODViNS04Mzc2MWU2OWYwYjYiLCJpYXQiOjE3MTg4NjA4MzUsImV4cCI6MTcxODk0NzIzNX0.pG1mgcmL2Kx2LWUopwze7DTnIhM8cECxfxVAaCsI8Go";
+  const companyCategoryUuid = "266db9b5-62c7-4e58-8e20-26d8d582a4f1";
+
+  useEffect(() => {
+    const fetchToken = async () => {
+      try {
+        const url = `${end_point}/auth/categorySetting/${companyCategoryUuid}`;
+        console.log("url:", url);
+        const response = await fetch(url, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (response.status === 200) {
+          const data = await response.json();
+          if (data && data.data && data.data.length > 0) {
+            const category = data.data[0];
+            const currentDate = new Date();
+            const startDate = new Date(category.startDate);
+            const endDate = new Date(category.endDate);
+            console.log("current date:", category);
+            if (
+              category.isEnable &&
+              category.service === "ESG新聞" &&
+              currentDate >= startDate &&
+              currentDate <= endDate
+            ) {
+              setIsValid(true);
+              onValidation(true);
+            } else {
+              setIsValid(false);
+              onValidation(false);
+            }
+          } else {
+            setIsValid(false);
+            onValidation(false);
+          }
+        } else {
+          setIsValid(false);
+          onValidation(false);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setIsValid(false);
+        onValidation(false);
+      }
+    };
+
+    fetchToken();
+  }, []);
+
+  return isValid;
+};
+
+export default useAuthCheck;
