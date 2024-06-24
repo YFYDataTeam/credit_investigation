5383dd72d47ae0c836c918736cf4b368b583b75c
sean.chang <sean.chang@yfy.com>
2024-06-24 08:45:40 +0800

    wip

--- frontend/src/MainPage/index.jsx
+++ frontend/src/MainPage/index.jsx
@@ -140,13 +140,13 @@ const App = () => {
 
         {finalCompanyId && <CddResult endPoint={endPoint} companyId={finalCompanyId}></CddResult>}
         
-        {finalCompanyId && <RevenueAnalysis endPoint={endPoint} companyId={finalCompanyId}></RevenueAnalysis>}
+        {/* {finalCompanyId && <RevenueAnalysis endPoint={endPoint} companyId={finalCompanyId}></RevenueAnalysis>} */}
 
-        {finalCompanyId && <FinancialReport endPoint={endPoint} companyId={finalCompanyId}></FinancialReport>}
+        {/* {finalCompanyId && <FinancialReport endPoint={endPoint} companyId={finalCompanyId}></FinancialReport>} */}
 
         {finalCompanyId && <JudgementSummary endPoint={endPoint} companyId={finalCompanyId}></JudgementSummary>}
 
-        {/* {finalCompanyId && <EpaReport endPoint={endPoint} companyId={finalCompanyId}></EpaReport>} */}
+        {finalCompanyId && <EpaReport endPoint={endPoint} companyId={finalCompanyId}></EpaReport>}
 
         {/* {finalCompanyId && <PstReport endPoint={endPoint} companyId={finalCompanyId}></PstReport>} */}
     </div>
--- frontend/src/common/components/utils/FinancialTable.jsx
+++ frontend/src/common/components/utils/FinancialTable.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import '../../assets/css/financialtable.css'
+import '../../../../assets/css/financialtable.css'
 
 const FinancialTable = ({ title, data }) => {
   return (
